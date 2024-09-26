67cd7326f0bf639234a50487389938a47de586e0
Awesometimei <dtnguyen194@student.hbuhsd.edu>
2024-09-26 10:17:04 -0700

    danny

--- Classwork.java
+++ Classwork.java
@@ -2,9 +2,26 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    System.out.println(firstLastN("dictionary", 1));
+    System.out.println(delEnd("bakaboy", 5));
   }
 
-  // write solutions to problems below
+  public static String firstLastN(String skibidi, int n)
+  {
+    String part1 = skibidi.substring(0, n);
+    String part2 = skibidi.substring(skibidi.length()-1);
+    return part1 + part2;
+  }
 
+  public static String delEnd(String sigma, int n)
+  {
+    String mwah = sigma.substring(0, (sigma.length()-n));
+    return mwah;
+  }
+
+  public static String compareLower(String sigma, String skibidi)
+  {
+
+    return sigma;
+  }
 }
